6dbd9fdcf38f5289860a000bcbbe51b7e47b0292
Nikita <nicitazaletin@yandex.ru>
2020-10-08 03:26:52 +0300

    lab_2_test

--- lab_2/exercise_1.asm
+++ lab_2/exercise_1.asm
@@ -8,6 +8,9 @@ Vec2: Times 60 dd     20000,        20010,        20020,        20030,        20
 
 length dw 300
 
+prefix dd 4278190080 ;
+perep dd 16777215 ;1ед,23 нуля в двоич. сс
+
 sum1 dd 0; первые 32 бита суммы
 sum2 dd 0; вторые 32 бита суммы
 
@@ -25,23 +28,46 @@ CMAIN:
     mov ecx, [length]
     
     l1:    
-        ;достаём элементы с одинауовым индексом
-        mov eax, [Vec1 + 4 * (ecx - 1)]
-        mov ebx, [Vec2 + 4 * (ecx - 1)]
+        
+        ;достаём элементы с одинаковым индексом
+        add eax, [Vec1 + 4 * (ecx - 1)]
+        add ebx, [Vec2 + 4 * (ecx - 1)]
         
         ;перемножаем их
         mul ebx
         mov ebx, eax
         
-        ;складываем с уже существующей суммой
-        mov eax, [sum1]
-        add eax, ebx
-        mov [sum1], eax
+        ;если число в edx >= perep
+        cmp edx, perep
+        JG lPerep
+        JMP l2
         
-        mov eax, [sum2]
-        adc eax, 0 
-        mov [sum2], eax
+        lPerep:
+            ;сдвигаем влево так, чтобы 24ый бит оказался на 32ой позиции
+            sal edx, 8
+            ;если edx >= этого числа(2^31), то на 32ой позиции 1, иначе 0
+            cmp edx, 2147483648
+            JG lUnit
+            sar edx, 8
+            JMP l2
+        
+        ;сейчас регистр имееи вид 8единиц потом наше число в 24ёх битах
+        lUnit:
+            ;обнуляемя 8мь единиц
+            add edx, 8388608
+        
+        l2:
+            ;складываем с уже существующей суммой
+            mov eax, [sum1]
+            add eax, ebx
+            mov [sum1], eax
+        
+            mov eax, [sum2]
+            adc eax, 0 
+            mov [sum2], eax
     loop l1
+    
+
     
     ;вывод
     mov eax, [sum1]

b52f3f562235ba7ae9de4240e65e1c0f2380bfca
Nikita <nicitazaletin@yandex.ru>
2020-11-05 17:15:10 +0300

    fix

--- lab_4/exercise_1.asm
+++ lab_4/exercise_1.asm
@@ -2,9 +2,9 @@
 
 section .data
 mask dd 2147483648 ;маска 10000000000000000000000000000000
-first dd 1 ;задаЄм первое число
-second dd 2 ;задаЄм второе число
-fird dd 3 ; задаЄм третье число
+first dd 4294967294 ;задаЄм первое число
+second dd 4294967295 ;задаЄм второе число
+fird dd 4294967293 ; задаЄм третье число
 
 cur_bit db 31 ; текущий сравниваемый бит
 result dq 0
@@ -21,61 +21,62 @@ CMAIN:
     mov ecx, 0
     mov edx, 0 
    
-
-    mov cx, 3     
+   
     mov eax, [mask] ; храним номер текущего бита с помощью маски
-    
+    mov ebx, [first]
+    mov ecx, [second]
+    mov edx, [fird]
+
     ;ќсновна€ иде€ - побитовое сранвение чисел, начина€ со старшего бита
     max: 
+    push ecx
+    push ebx
     cmp eax, 0 ;если eax = 0, то все биты 2-х чисел одинаковые
     je bxb
     
-    mov ebx, [first]
-    mov edx, [second]
-    
     ;выдел€ем нужный бит 
     and ebx, eax
-    and edx, eax
+    and ecx, eax
     
     ;ѕровер€ем, будут ли текущие биты равны
-    xor ebx, edx
+    xor ebx, ecx
     cmp ebx, 0
     je le 
     
-    ;если оказалось, что они не равны, то бит в одном из чисел 1, в другом 0. ¬ edx бит не помен€лс€
-    cmp edx, 0
-    jne dxb 
+    ;если оказалось, что они не равны, то бит в одном из чисел 1, в другом 0. ¬ ecx бит не помен€лс€
+    cmp ecx, 0
+    jne cxb 
     jmp bxb 
          
                            
-    le: ;le - label equal
+    le: ; le - label equal
         shr eax, 1
+        pop ebx
+        pop ecx
         jmp max
     
-    dxb: ; dx - edx bigger
-        mov eax, [second]
-        mov [result], eax
+    cxb: ; cx - ecx bigger
+        pop ebx
+        pop ecx
+        mov [result], ecx
         jmp c2
     
     bxb: ; bx - ebx bigger
-        mov eax, [first]
+        pop eax
         mov [result], eax
+        pop eax
         
     c2: 
         cmp dword [flag], 1
         je end
         
-        ;в first загружаем наибольшее между 1 и 2 числами
-        mov eax, [result]
-        mov [first], eax
-        ;в second загружаем 3-е число
-        mov eax, [fird]
-        mov [second], eax
-        
-        
+        mov ebx, [result] ;в ebx загружаем наибольшее между 1 и 2 числами
+        mov ecx, edx ;в ecx загружаем 3-е число
+          
         mov dword [flag], 1
         mov eax, [mask]
         jmp max
+ 
      
     end:     
     PRINT_UDEC 4, [result]
